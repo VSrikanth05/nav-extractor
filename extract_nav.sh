9414803d0023c3922ab51e59a79c2410a7e2e529
VSrikanth05 <vallurisrikanth05@gmail.com>
2025-05-08 15:48:00 +0530

    Add script to extract Scheme Name and Asset Value from AMFI NAV data

---
+++ extract_nav.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+# Download the NAVAll.txt file
+curl -s https://www.amfiindia.com/spages/NAVAll.txt -o NAVAll.txt
+
+# Extract Scheme Name and Asset Value, save as TSV
+awk -F';' 'NF>=6 && $1 !~ /Scheme Code/ {print $4 "\t" $6}' NAVAll.txt > nav_data.tsv
+
+# Clean up
+rm NAVAll.txt
